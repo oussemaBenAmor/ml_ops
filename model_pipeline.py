b85eb9636cc1557b69f777a829bc18a353163fec
oussemaBenAmor <oussema.benamor@esprit.tn>
2025-02-22 00:28:04 +0100

    mlflow

--- model_pipeline.py
+++ model_pipeline.py
@@ -256,12 +256,3 @@ def retraine_svm(C, kernel, degree, gamma, coef0, random_state):
 
     # Return response
     return accuracy, precision, recall, f1
-        mlflow.log_metric("f1_score", f1)
-	local_storage_path = r"C:\Users\MSI\Desktop\ml_ops\ml\project\svm_model.joblib"
-    # Save the model locally
-    dump(model, local_storage_path)
-
-    # Return response
-    return accuracy, precision, recall, f1
-    
-    
